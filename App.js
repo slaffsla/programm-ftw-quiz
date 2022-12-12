980b0f7be6bf80ce70b709343d14d0e681467433
Slava <slasla@gmail.com>
2022-12-13 00:42:34 +0200

    make russian translation work, kinda

--- App.js
+++ App.js
@@ -1,27 +1,39 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './components/pages/Home';
-import ChooseRoadMap from './components/pages/ChooseRoadMap';
-import Welcome from './components/pages/Welcome';
-import ReactPage from './components/pages/ReactPage';
-import AngularPage from './components/pages/AngularPage';
-import HtmlPage from './components/pages/HtmlPage';
-import Quiz from './components/pages/Quiz';
-import About from './components/pages/About';
-import TopMenuBar from './components/TopMenuBar';
-import HelpPage from './components/pages/Help';
-import TestPage from './components/pages/TestPage';
-import HomePage from './components/pages/HomePage';
+import * as React from "react";
+import i18next from "./assets/i18n/i18n";
+window.Array({ i18next });
+import { useTranslation } from "react-i18next";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./components/pages/Home";
+import ChooseRoadMap from "./components/pages/ChooseRoadMap";
+import Welcome from "./components/pages/Welcome";
+import ReactPage from "./components/pages/ReactPage";
+import AngularPage from "./components/pages/AngularPage";
+import HtmlPage from "./components/pages/HtmlPage";
+import Quiz from "./components/pages/Quiz";
+import About from "./components/pages/About";
+import TopMenuBar from "./components/TopMenuBar";
+import HelpPage from "./components/pages/Help";
+import TestPage from "./components/pages/TestPage";
+import HomePage from "./components/pages/HomePage";
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  //const [currentLanguage, setLanguage] = React.useState("en");
+  //console.log(currentLanguage);
+  const changeLanguage = (value) => {
+    i18next
+      .changeLanguage(value)
+      .then(() => setLanguage(value))
+      .catch((err) => console.log(err));
+  };
   return (
     <View style={styles.container}>
-
-      <NavigationContainer style={{ position: 'absolute', zIndex: 1 }}>
-        <Stack.Navigator >
+      <NavigationContainer style={{ position: "absolute", zIndex: 1 }}>
+        <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Welcome" component={Welcome} />
           <Stack.Screen name="ChooseRoadMap" component={ChooseRoadMap} />
@@ -30,17 +42,16 @@ function App() {
           <Stack.Screen name="HtmlPage" component={HtmlPage} />
           <Stack.Screen name="Quiz" component={Quiz} />
           <Stack.Screen name="AboutPage" component={About} />
-           <Stack.Screen name="HelpPage" component={HelpPage} />
+          <Stack.Screen name="HelpPage" component={HelpPage} />
         </Stack.Navigator>
-
       </NavigationContainer>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#18C5C5',
-    flex: 6
-  }
+    backgroundColor: "#18C5C5",
+    flex: 6,
+  },
 });
 export default App;
--- components/pages/Home.js
+++ components/pages/Home.js
@@ -1,37 +1,78 @@
-import { Text, View, StyleSheet, Image, Button, TextInput, TouchableOpacity,ImageBackground } from 'react-native';
-import TopMenuBar from '../TopMenuBar';
-import LoginConnect from '../loginconnect';
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import TopMenuBar from "../TopMenuBar";
+import LoginConnect from "../loginconnect";
+import { useTranslation } from "react-i18next";
 
-export default function Home({ navigation }) {
+export default function Home({ navigation, lang }) {
+  const { t, i18n } = useTranslation();
+
+  const [currentLanguage, setLanguage] = React.useState("en");
+  console.log(i18n);
+  console.log(currentLanguage);
+  const changeLanguage = (value) => {
+    i18n
+      .changeLanguage(value)
+      .then(() => setLanguage(value))
+      .catch((err) => console.log(err));
+  };
   return (
     <View>
       <TopMenuBar {...{ navigation }} />
       <View style={styles.container}>
         <Text style={styles.paragraph}>
           WELCOME !!!
-{"\n"}PROGRAMM FTW
           {"\n"}
-          CANDIDATE!</Text>
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Welcome')}>
-          <Image style={styles.startBtn} source={require("../../assets/icons/startBtn.png")} />
+          {t("hello")} {"\n"}PROGRAMM FTW
+          {"\n"}
+          CANDIDATE!
+        </Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Welcome")}>
+          <Image
+            style={styles.startBtn}
+            source={require("../../assets/icons/startBtn.png")}
+          />
         </TouchableOpacity>
 
-
+        <Pressable
+          onPress={() => changeLanguage("en")}
+          style={{
+            padding: 20,
+          }}
+        >
+          <Text>Select English</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => changeLanguage("ru")}
+          style={{
+            padding: 20,
+          }}
+        >
+          <Text>Select Russian</Text>
+        </Pressable>
       </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#18C5C5',
-    height: 590
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#18C5C5",
+    height: 590,
   },
-  coverImage:{
-      width: 300,
+  coverImage: {
+    width: 300,
     height: 200,
     resizeMode: "stretch",
   },
@@ -43,8 +84,8 @@ const styles = StyleSheet.create({
     margin: 24,
     fontSize: 18,
     color: "#001a00",
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: -25
-  }
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: -25,
+  },
 });
